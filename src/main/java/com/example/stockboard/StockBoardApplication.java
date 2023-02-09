1a02531cc5546a2b021712cb013b344bb6bbdd28
임기원 <gwim1130@naver.com>
2023-02-09 11:20:23 +0900

    프로젝트 세팅중

---
+++ src/main/java/com/example/stockboard/StockBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.stockboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockBoardApplication.class, args);
+    }
+
+}
